d3ef9abcbc47873f2bb7ba1cb36c135d743d2648
Sway-Tree <agnieszka.dudek92@gmail.com>
2013-10-19 18:54:54 +0000

    Preventing trolls from extinction Comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+
+	has_many :comments, dependent: :destroy
+
 	validates :title, presence: true
     validates :text, presence: true,
                     length: { minimum: 5 }
